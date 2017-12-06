ef6d5e1c6fc548de609c841e45c778dcd69c5397
qlam699 <quanglam.thieu@yahoo.com>
2017-12-06 23:00:19 +0700

    server-setup

---
+++ config/database.js
@@ -0,0 +1,10 @@
+const crypto = require('crypto')
+    .randomBytes(256)
+    .toString('hex'); // Provides cryptographic functionality (OpenSSL's hash, HMAC, cipher, decipher, sign and verify functions)
+
+
+module.exports = {
+    uri: 'mongodb://localhost:27017/' + this.db,
+    secret: crypto,
+    db: 'postmean'
+}
